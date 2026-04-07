622d1258484bcf95f868a8cda7be26d384cb98aa
Shomari-Thompson <shomarit1@gmail.com>
2026-04-06 22:29:00 -0400

    Completed endpoint registration

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.db.database import Base, engine
+from app.models.user import User
 from app.routes.auth import router as auth_router
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 @app.get("/")
--- server/app/routes/auth.py
+++ server/app/routes/auth.py
@@ -1,7 +1,32 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.schemas.user import UserRegister
+from app.models.user import User 
+from app.db.database import get_db
+from app.core.security import hash_password
+from app.schemas.user import UserRegister
 
 router = APIRouter(prefix="/auth", tags =["auth"])
 
-@router.get("/test")
-def auth_test():
-    return{"message": "auth route working"}
+@router.post("/register")
+def register(user: UserRegister, db: Session = Depends(get_db)):
+    
+    #check if user exists
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    # Hash Password
+    hashed_password = hash_password(user.password)
+
+    #create user
+    new_user = User(
+        full_name=user.full_name,
+        email=user.email,
+        password_hash=hashed_password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"Message: User created Successfully"}
